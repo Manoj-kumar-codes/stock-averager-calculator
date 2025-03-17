028a0b003b27c0e5023e84f9370a5475bb31859c
Manoj-kumar-codes <manojkumar.codes@gmail.com>
2025-03-17 18:11:06 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -8,18 +8,28 @@ const resetFun = function () {
   document.getElementById("price2").value = "";
   document.getElementById("share2").value = "";
 };
-resetFun();
 calculator.addEventListener("click", function () {
   const price1 = Number(document.getElementById("price1").value);
   const share1 = Number(document.getElementById("share1").value);
   const price2 = Number(document.getElementById("price2").value);
   const share2 = Number(document.getElementById("share2").value);
+
   const totalShares = document.getElementById("total-share");
   const totalPrices = document.getElementById("total-price");
   const avgPrices = document.getElementById("total-avg");
 
-  if (!price1 && !share1 && !price2 && !share2) {
-    alert(`Don't submit the empty field`);
+  if (
+    document.getElementById("price1").value.trim() === "" ||
+    document.getElementById("share1").value.trim() === "" ||
+    document.getElementById("price2").value.trim() === "" ||
+    document.getElementById("share2").value.trim() === "" ||
+    isNaN(price1) ||
+    isNaN(share1) ||
+    isNaN(price2) ||
+    isNaN(share2)
+  ) {
+    alert("Please enter valid numbers");
+    return;
   } else {
     const avg = (price1 * share1 + price2 * share2) / (share1 + share2);
     const totalShare = share1 + share2;
